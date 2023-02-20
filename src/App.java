166494f2da9c507c3bde7c1e50c9c4e207d838a9
kisgyorgybeni <kisgyorgybeni@gmail.com>
2023-02-20 12:57:44 +0100

    kezdes

---
+++ src/App.java
@@ -0,0 +1,12 @@
+/*
+* File: App.java
+* Author: Kondor Marcell
+* Group: Szoft 1/n
+* Date: 2023-02-20
+* Github: https://github.com/janos/
+*/
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
